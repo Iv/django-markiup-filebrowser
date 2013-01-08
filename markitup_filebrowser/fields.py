a12e3f0de9e8c10c279d795744f87b7e716bd34c
Ivan Kravchenko <iv.kravchenko@gmail.com>
2013-01-09 01:34:08 +0400

    Allow south to handle MarkupFilebrowserFiled

--- markitup_filebrowser/fields.py
+++ markitup_filebrowser/fields.py
@@ -10,3 +10,18 @@ class MarkupFilebrowserFiled(MarkupField):
 
 from django.contrib.admin.options import FORMFIELD_FOR_DBFIELD_DEFAULTS
 FORMFIELD_FOR_DBFIELD_DEFAULTS[MarkupFilebrowserFiled] = {'widget': widgets.AdminMarkitUpFilebrowserWiget}
+
+
+# allow South to handle MarkupField smoothly
+try:
+    from south.modelsinspector import add_introspection_rules
+    # For a normal MarkupField, the add_rendered_field attribute is
+    # always True, which means no_rendered_field arg will always be
+    # True in a frozen MarkupField, which is what we want.
+    add_introspection_rules(rules=[((MarkupFilebrowserFiled,),
+                                    [],
+                                    {'no_rendered_field': ('add_rendered_field',
+                                                           {})})],
+        patterns=['markitup_filebrowser\.fields\.'])
+except ImportError:
+    pass
